3a7ec313cba3d0b6aab7f4d1db7d3521c9536730
anurag-512 <anuragsharan51@gmail.com>
2024-07-15 11:23:18 +0530

    back to top button added

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,18 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("clicked");
     body.classList.toggle("overflow-hidden");
 });
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
