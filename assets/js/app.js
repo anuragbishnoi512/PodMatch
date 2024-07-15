27c970aa012f068515afae499cef390aa8fcc9ac
anurag-512 <anuragsharan51@gmail.com>
2024-07-15 10:42:57 +0530

    responsive done till @576

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,9 @@ function opennav() {
     document.querySelector(".close").classList.toggle("start-0")
 }
 let hamburger = document.querySelector(".hamburger");
+let body = document.body
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("clicked");
+    body.classList.toggle("overflow-hidden");
 });
